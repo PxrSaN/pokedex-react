be0526060d6ecbdf206f77c34d47fab9c9b7eeff
PxrSaN <zackquinn@outlook.es>
2023-06-09 17:34:05 -0500

    cambio de loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,17 @@
-import { DotSpinner } from '@uiball/loaders';
+import { JellyTriangle } from '@uiball/loaders'
+
+
 
 export const Loader = () => {
 	return (
         <div className="container-loader">
-            <DotSpinner size={40} speed={0.9} color='black' />;
+            
+
+<JellyTriangle 
+ size={60}
+ speed={1.75} 
+ color="black" 
+/>
         </div>
     )
 };
