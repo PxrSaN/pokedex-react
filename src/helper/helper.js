22c999bea33fa22981461e4291c31f093841bf35
PxrSaN <zackquinn@outlook.es>
2023-06-16 17:56:13 -0500

    ''

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,3 +1,5 @@
+/*Con esta funcion haremos que la primer letra de los pokemon se convierta en mayuscula
+ya que la API los devuelve en minuscula*/
 export const primerMayuscula = (word) => {
 return word[0].toUpperCase() + word.substring(1)
 }
